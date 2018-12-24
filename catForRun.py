eaf9abd329979cd97f7bed418972898a7196b100
jblake95 <J.Blake@warwick.ac.uk>
2018-12-24 21:01:37 +0000

    Need to create TLE class

--- catForRun.py
+++ catForRun.py
@@ -3,6 +3,7 @@ Obtain overall 3LE catalog for a given date range
 """
 
 import argparse as ap
+import getpass as gp
 from spacetrack import SpaceTrackClient
 import spacetrack.operators as op
 
@@ -35,6 +36,58 @@ def argParse():
     
     return parser.parse_args()
 
+class ST:
+	"""
+	Space-Track Interface
+	"""
+	def __init__(self):
+		un, pw = self.requestAccess()
+		self.username = un
+		self.password = pw
+		self.client = SpaceTrackClient(identity=un, password=pw)
+	
+	def requestAccess(self):
+		"""
+		Obtain user access details
+		"""
+		st_un = 'J.Blake@warwick.ac.uk'
+		st_pw = gp.getpass('Space-Track password: ')
+		return st_un, st_pw
+	
+	def getLatestCatalog(self, out_dir=None, le_format='3le'):
+		"""
+		Obtain catalog of latest TLEs from Space-Track
+		"""
+		return self.client.tle_latest(iter_lines=True, 
+                                      epoch='>now-30', 
+                                      ordinal=1, 
+                                      format=le_format)
+    
+    def getPastCatalog(self, start, end, out_dir=None, le_format='3le'):
+		"""
+		Obtain catalog of TLEs for a given epoch range
+		"""
+		
+		return self.client.tle_latest(iter_lines=True, 
+                                      epoch='>now-30', 
+                                      ordinal=1, 
+                                      format=le_format)
+
+class TLE:
+	"""
+	Two Line Element
+	"""
+
 if __name__ == "__main__":
+	
+	#args = argParse()
+	
+	st = ST()
+	print(st.client)
+	
+	cat = st.getLatestCatalog(le_format='3le')
+	
+	for c in cat:
+		print(c)
 	
 	
