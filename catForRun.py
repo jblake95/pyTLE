c4d4ecd55946351bbf2faac125835aadd0ab6caf
jblake95 <J.Blake@warwick.ac.uk>
2018-12-25 00:28:12 +0000

    Need to add method for past catalog

--- catForRun.py
+++ catForRun.py
@@ -6,6 +6,16 @@ import argparse as ap
 import getpass as gp
 from spacetrack import SpaceTrackClient
 import spacetrack.operators as op
+import datetime
+from skyfield.api import load, Topos, utc
+from skyfield.sgp4lib import EarthSatellite
+
+SITE_LATITUDE = '28.7603135N'
+SITE_LONGITUDE = '17.8796168 W'
+SITE_ELEVATION = 2387
+TOPOS_LOCATION = Topos(SITE_LATITUDE, 
+                       SITE_LONGITUDE, 
+                       elevation_m=SITE_ELEVATION)
 
 def argParse():
     """
@@ -54,6 +64,15 @@ class ST:
 		st_pw = gp.getpass('Space-Track password: ')
 		return st_un, st_pw
 	
+	def getLatestTLE(self, norad_id, le_format='3le'):
+		"""
+		Obtain latest TLE for a given NORAD object
+		"""
+		return self.client.tle_latest(norad_cat_id=norad_id,
+		                              iter_lines=True,
+		                              ordinal=1,
+		                              format=le_format)
+	
 	def getLatestCatalog(self, out_dir=None, le_format='3le'):
 		"""
 		Obtain catalog of latest TLEs from Space-Track
@@ -77,17 +96,29 @@ class TLE:
 	"""
 	Two Line Element
 	"""
+	def __init__(self, line1, line2, name=None):
+		self.line1 = line1
+		self.line2 = line2
+		if name is not None:
+			self.name = name
+		self.obs = TOPOS_LOCATION
+        self.obj = EarthSatellite(tle_array[1], tle_array[2])
+        self.ts = load.timescale()
+	
+	def radec(self, epoch):
+		"""
+		Determine radec coords for a given epoch
+		"""
+		ra, dec, _ = (self.obj - self.obs).at(self.ts.utc(time)).radec()
+        return ra._degrees * u.degree, dec.degrees * u.degree
+		
+		
 
 if __name__ == "__main__":
 	
 	#args = argParse()
 	
-	st = ST()
-	print(st.client)
 	
-	cat = st.getLatestCatalog(le_format='3le')
-	
-	for c in cat:
-		print(c)
+	tle = TLE()
 	
 	
