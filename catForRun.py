77f537a6cdc3624e6f8d298aa5bdfdd9c92ee2f8
jblake95 <J.Blake@warwick.ac.uk>
2019-02-04 15:54:27 +0000

    Moving classes to separate module

--- catForRun.py
+++ catForRun.py
@@ -2,29 +2,10 @@
 Obtain overall 3LE catalog for a given date range
 """
 
+from tle import TLE, ST
 import json
-import argparse as ap
-import getpass as gp
-from spacetrack import SpaceTrackClient
-import spacetrack.operators as op
 import datetime
-from skyfield.api import load, Topos, utc
-from skyfield.sgp4lib import EarthSatellite
-
-TS = load.timescale()
-
-SITE_LATITUDE = '28.7603135N'
-SITE_LONGITUDE = '17.8796168W'
-SITE_ELEVATION = 2387
-TOPOS_LOCATION = Topos(SITE_LATITUDE, 
-                       SITE_LONGITUDE, 
-                       elevation_m=SITE_ELEVATION)
-
-GEO_CHECK = ['g', 'geo']
-LEO_CHECK = ['l', 'leo']
-MEO_CHECK = ['m', 'meo']
-HEO_CHECK = ['h', 'heo']
-ALL_CHECK = ['a', 'all']
+import argparse as ap
 
 def argParse():
     """
@@ -54,157 +35,6 @@ def argParse():
                         type=str)
     
     return parser.parse_args()
-
-class Orbit:
-    """
-    Convenience class for orbit-specific searches
-    """
-    def __init__(self, orb_type):
-        """
-        Initiate Orbit object using SpaceTrack definitions
-        
-        Parameters
-        ----------
-        orb_type : str
-            Desired type of orbit
-            'g' - GEO
-            'l' - LEO
-            'm' - MEO
-            'h' - HEO
-            'a' - ALL
-        """
-        if orb_type.lower() in GEO_CHECK:
-            self.eccentricity_lim = '<0.01'
-            self.mean_motion_lim = '0.99--1.01'
-        elif orb_type.lower() in LEO_CHECK:
-            self.eccentricity_lim = '<0.25'
-            self.mean_motion_lim = '>11.25'
-        elif orb_type.lower() in MEO_CHECK:
-            self.eccentricity_lim = '<0.25'
-            self.period_lim = '600--800'
-        elif orb_type.lower() in HEO_CHECK:
-            self.eccentricity_lim = '>0.25'
-        elif orb_type.lower() in ALL_CHECK:
-            print('Full catalogue specified; no limits placed.')
-        else:
-            print('Please provide a valid orbit type\n'
-                  'GEO - "g"\n'
-                  'LEO - "l"\n'
-                  'MEO - "m"\n'
-                  'HEO - "h"\n'
-                  'ALL - "a"\n')
-            quit()
-
-class ST:
-    """
-    Space-Track Interface
-    """
-    def __init__(self):
-        un, pw = self.requestAccess()
-        self.username = un
-        self.password = pw
-        self.client = SpaceTrackClient(identity=un, password=pw)
-    
-    def requestAccess(self):
-        """
-        Obtain user access details
-        """
-        st_un = 'J.Blake@warwick.ac.uk'
-        st_pw = gp.getpass('Space-Track password: ')
-        return st_un, st_pw
-    
-    def getLatestTLE(self, norad, le_format='3le'):
-        """
-        Obtain latest TLE for a NORAD object
-        """
-        return self.client.tle_latest(norad_cat_id=norad,
-                                      iter_lines=True,
-                                      ordinal=1,
-                                      format=le_format)
-    
-    def getLatestCatalog(self, out_dir=None, le_format='3le'):
-        """
-        Obtain catalog of latest TLEs from Space-Track
-        """
-        return self.client.tle_latest(iter_lines=True, 
-                                      epoch='>now-30', 
-                                      ordinal=1, 
-                                      format=le_format)
-    
-    def getPastTLE(self, norad, start, end, epoch=None, le_format='3le'):
-        """
-        Obtain list of TLEs for a NORAD object within an epoch range,
-        narrowed down to one (most recent) if desired epoch given
-        """
-        
-        
-        return
-    
-    def getRunCatGEO(self, start, end, out_dir=None, le_format='3le'):
-        """
-        Obtain catalog of GEO TLEs for a given epoch range
-        
-        Parameters
-        ----------
-        start, end : str
-            Start and end epochs for search, format 'yyyy-mm-dd'
-        out_dir : str, optional
-            Output directory in which to store catalogue
-            Default = None
-        le_format : str
-            Format of returned element sets ('tle' or '3le')
-            Default = '3le'
-        
-        Returns
-        -------
-        tles : array-like
-            Element sets returned from query to SpaceTrack
-        """
-        orb = Orbit('geo') 
-        result = self.client.tle(iter_lines=True,
-                                 eccentricity=orb.eccentricity_lim,
-                                 mean_motion=orb.mean_motion_lim,
-                                 epoch='{}--{}'.format(start, end),
-                                 format=le_format)
-        tles = [line for line in result]
-        
-        if out_dir is not None:
-            with open(out_dir + 'run_catalogue.txt', 'w') as f:
-                for line in tles:
-                    f.write('{}\n'.format(line))
-        
-        return tles
-
-class TLE:
-    """
-    Two Line Element
-    """
-    def __init__(self, line1, line2, name=None):
-        self.line1 = line1
-        self.line2 = line2
-        if name is not None:
-            self.name = name[2:]
-        
-        self.obs = TOPOS_LOCATION
-        self.obj = EarthSatellite(line1, line2, name)
-        self.ts = TS
-        
-        self.norad_id = int(self.line1[2:7])
-        self.yearday = float(self.line1[20:32])
-        
-        self.inclination = float(self.line2[8:16])
-        self.eccentricity = float(self.line2[26:33])
-        self.raan = float(self.line2[17:25])
-        self.argperigree = float(self.line2[34:42])
-        self.mean_anomaly = float(self.line2[43:51])
-        self.mean_motion = float(self.line2[52:63])
-    
-    def radec(self, epoch):
-        """
-        Determine radec coords for a given epoch
-        """
-        ra, dec, _ = (self.obj - self.obs).at(self.ts.utc(time)).radec()
-        return ra._degrees * u.degree, dec.degrees * u.degree  
 
 def getYearDay(epoch):
     """
@@ -256,7 +86,7 @@ if __name__ == "__main__":
     
     run_cat = st.getRunCatGEO(args.start,
                               args.end)
-                              args.out_dir)
+                              #args.out_dir)
     
     print(len(run_cat))
     
--- tle.py
+++ tle.py
@@ -1,56 +1,175 @@
 """
-Useful functions for dealing with Space-Track two-line elements
+Module for dealing with Space-Track elsets
 """
 
+import getpass as gp
 from spacetrack import SpaceTrackClient
 import spacetrack.operators as op
+from skyfield.api import load, Topos, utc
+from skyfield.sgp4lib import EarthSatellite
 
-def getSatCat(name, pw, epoch='>now-30', le_format='3le', save=True):
+TS = load.timescale()
+
+SITE_LATITUDE = '28.7603135N'
+SITE_LONGITUDE = '17.8796168W'
+SITE_ELEVATION = 2387
+TOPOS_LOCATION = Topos(SITE_LATITUDE, 
+                       SITE_LONGITUDE, 
+                       elevation_m=SITE_ELEVATION)
+
+GEO_CHECK = ['g', 'geo']
+LEO_CHECK = ['l', 'leo']
+MEO_CHECK = ['m', 'meo']
+HEO_CHECK = ['h', 'heo']
+ALL_CHECK = ['a', 'all']
+
+class Orbit:
     """
-    Retrieves full catalogue of latest elsets in requested format
-    
-    Parameters
-    ----------
-    name : str
-        Username for access to the Space-Track database
-    pw : str
-        Password for access to the Space-Track database
-    epoch : str, optional
-        Dates from which to pull elsets. Can specify a period of time
-        using '--', for example '2017-01-01--2017-12-08', will give 
-        elsets that have updated within that epoch range
-        Default = '>now-30' (for latest elsets)
-    le_format : str
-        Format of the retrieved line element sets
-        'tle' - returns two line elements
-        '3le' - returns three line elements (includes sat names)
-        Default = '3le'
-    save : bool, optional
-        Toggle to save catalogue to file
-        Default = True
-    
-    Returns
-    -------
-    None
-    """ 
-    st = SpaceTrackClient(identity=name, password=pw)
-    
-    cat = st.tle_latest(iter_lines=True, 
-                        epoch=epoch, 
-                        ordinal=1, 
-                        format=le_format)
-    
-    if save:
-		with open('tle_latest.txt', 'w') as f:
-			for line in data:
-				f.write(line + '\n')
-    
-    return cat
+    Convenience class for orbit-specific searches
+    """
+    def __init__(self, orb_type):
+        """
+        Initiate Orbit object using SpaceTrack definitions
+        
+        Parameters
+        ----------
+        orb_type : str
+            Desired type of orbit
+            'g' - GEO
+            'l' - LEO
+            'm' - MEO
+            'h' - HEO
+            'a' - ALL
+        """
+        if orb_type.lower() in GEO_CHECK:
+            self.eccentricity_lim = '<0.01'
+            self.mean_motion_lim = '0.99--1.01'
+        elif orb_type.lower() in LEO_CHECK:
+            self.eccentricity_lim = '<0.25'
+            self.mean_motion_lim = '>11.25'
+        elif orb_type.lower() in MEO_CHECK:
+            self.eccentricity_lim = '<0.25'
+            self.period_lim = '600--800'
+        elif orb_type.lower() in HEO_CHECK:
+            self.eccentricity_lim = '>0.25'
+        elif orb_type.lower() in ALL_CHECK:
+            print('Full catalogue specified; no limits placed.')
+        else:
+            print('Please provide a valid orbit type\n'
+                  'GEO - "g"\n'
+                  'LEO - "l"\n'
+                  'MEO - "m"\n'
+                  'HEO - "h"\n'
+                  'ALL - "a"\n')
+            quit()
 
-def getTLE(norad_id, epoch):
-	"""
-	Obtain a TLE for a given object within a desired epoch range
-	"""
-	st.tle(norad_cat_id=25544, orderby='epoch desc', limit=22, format='tle')
-	
-	return tle
+class ST:
+    """
+    Space-Track Interface
+    """
+    def __init__(self):
+        un, pw = self.requestAccess()
+        self.username = un
+        self.password = pw
+        self.client = SpaceTrackClient(identity=un, password=pw)
+    
+    def requestAccess(self):
+        """
+        Obtain user access details
+        """
+        st_un = 'J.Blake@warwick.ac.uk'
+        st_pw = gp.getpass('Space-Track password: ')
+        return st_un, st_pw
+    
+    def getLatestTLE(self, norad, le_format='3le'):
+        """
+        Obtain latest TLE for a NORAD object
+        """
+        return self.client.tle_latest(norad_cat_id=norad,
+                                      iter_lines=True,
+                                      ordinal=1,
+                                      format=le_format)
+    
+    def getLatestCatalog(self, out_dir=None, le_format='3le'):
+        """
+        Obtain catalog of latest TLEs from Space-Track
+        """
+        return self.client.tle_latest(iter_lines=True, 
+                                      epoch='>now-30', 
+                                      ordinal=1, 
+                                      format=le_format)
+    
+    def getPastTLE(self, norad, start, end, epoch=None, le_format='3le'):
+        """
+        Obtain list of TLEs for a NORAD object within an epoch range,
+        narrowed down to one (most recent) if desired epoch given
+        """
+        
+        
+        return
+    
+    def getRunCatGEO(self, start, end, out_dir=None, le_format='3le'):
+        """
+        Obtain catalog of GEO TLEs for a given epoch range
+        
+        Parameters
+        ----------
+        start, end : str
+            Start and end epochs for search, format 'yyyy-mm-dd'
+        out_dir : str, optional
+            Output directory in which to store catalogue
+            Default = None
+        le_format : str
+            Format of returned element sets ('tle' or '3le')
+            Default = '3le'
+        
+        Returns
+        -------
+        tles : array-like
+            Element sets returned from query to SpaceTrack
+        """
+        orb = Orbit('geo') 
+        result = self.client.tle(iter_lines=True,
+                                 eccentricity=orb.eccentricity_lim,
+                                 mean_motion=orb.mean_motion_lim,
+                                 epoch='{}--{}'.format(start, end),
+                                 format=le_format)
+        tles = [line for line in result]
+        
+        if out_dir is not None:
+            with open(out_dir + 'run_catalogue.txt', 'w') as f:
+                for line in tles:
+                    f.write('{}\n'.format(line))
+        
+        return tles
+
+class TLE:
+    """
+    Two Line Element
+    """
+    def __init__(self, line1, line2, name=None):
+        self.line1 = line1
+        self.line2 = line2
+        if name is not None:
+            self.name = name[2:]
+        
+        self.obs = TOPOS_LOCATION
+        self.obj = EarthSatellite(line1, line2, name)
+        self.ts = TS
+        
+        self.norad_id = int(self.line1[2:7])
+        self.yearday = float(self.line1[20:32])
+        
+        self.inclination = float(self.line2[8:16])
+        self.eccentricity = float(self.line2[26:33])
+        self.raan = float(self.line2[17:25])
+        self.argperigree = float(self.line2[34:42])
+        self.mean_anomaly = float(self.line2[43:51])
+        self.mean_motion = float(self.line2[52:63])
+    
+    def radec(self, epoch):
+        """
+        Determine radec coords for a given epoch
+        """
+        ra, dec, _ = (self.obj - self.obs).at(self.ts.utc(time)).radec()
+        return ra._degrees * u.degree, dec.degrees * u.degree  
